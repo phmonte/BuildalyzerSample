7642b3468850cc2ef2a84dd51c1197d49d45bd2b
Pablo Monteiro <pablohenriquem@hotmail.com>
2024-04-13 01:38:30 -0300

    fix: simulation of scenario 224 - error when building with publish profile

---
+++ ClassLibrary1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Buildalyzer;
+
+var manager = new AnalyzerManager();
+var analyzer = manager.GetProject(@"../../../../ClassLibrary1/ClassLibrary1.csproj");
+
+Console.WriteLine("1");
+
+_ = analyzer.Build();
+
+Console.WriteLine("2");
